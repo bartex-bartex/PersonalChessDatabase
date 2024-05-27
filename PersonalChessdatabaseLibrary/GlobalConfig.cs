c18c42c3717462551fe85127f83a17f15883d6bb
Bartosz Warchoł <bartex132@gmail.com>
2024-05-27 12:16:01 +0200

    Added basic Data Access Configuration

---
+++ PersonalChessdatabaseLibrary/GlobalConfig.cs
@@ -0,0 +1,30 @@
+﻿using PersonalChessdatabaseLibrary.DataAccess;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalChessdatabaseLibrary
+{
+    public class GlobalConfig
+    {
+        public static IDataConnection Connection { get; private set; }
+
+        public static void InitializeConnections(DataSourceType dataSource)
+        {
+            if (dataSource == DataSourceType.Sql)
+            {
+                SqlConnector sql = new SqlConnector();
+                Connection = sql;
+            }
+        }
+
+        public static string CnnString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
